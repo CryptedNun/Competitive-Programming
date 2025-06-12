6a3bcb31897830f5a6e0517ff7333587e9b0b575
CryptedNun <hasanmahmudbd2005@gmail.com>
2025-06-12 17:50:42 +0600

    Add GCD Algorithm

---
+++ CP-Algorithms/BinaryExponentiation/EuclideanGCD.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// gdc(a, b) = gcd(b, a%b);
+int gcd(int a, int b) {
+    while(b != 0) {
+        int temp = a;
+        a = b;
+        b = temp%b;
+    }
+    return a;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int m,n; cin>>m>>n;
+    cout<<gcd(m, n);
+}
+
