e1bdf7578a279a96058a70c13ec2d0d2d0317580
CryptedNun <hasanmahmudbd2005@gmail.com>
2025-06-16 10:45:29 +0600

    Add extra note to CF-1916B

--- Solves/CodeForces/1000/1916B.cpp
+++ Solves/CodeForces/1000/1916B.cpp
@@ -15,3 +15,6 @@ int main() {
         }
     }
 }
+
+// ! One thing to note, the first case doesn't work for all a and b. 
+// ! Suppose 15 and 30, gives 60. But 30 and 20 are its biggest divisors. Hence for 15 and 30, no such x exists.
