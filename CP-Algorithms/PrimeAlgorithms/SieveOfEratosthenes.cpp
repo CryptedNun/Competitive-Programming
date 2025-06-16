46ad090688646a0f8d2082c63ea92f6ff9f6abd7
CryptedNun <hasanmahmudbd2005@gmail.com>
2025-06-16 13:38:06 +0600

    Add optimized Sieve

--- CP-Algorithms/PrimeAlgorithms/SieveOfEratosthenes.cpp
+++ CP-Algorithms/PrimeAlgorithms/SieveOfEratosthenes.cpp
@@ -15,6 +15,32 @@ vector<bool> sieve(long long n) {
     return is_prime;
 }
 
+// ! Further optimized code, returns all primes instead of a bool array
+// vector<long long> sieve(long long n) {
+//     vector<bool> is_prime(n + 1, true);
+//     is_prime[0] = is_prime[1] = false;
+
+//     vector<long long> primes;
+//     if (n >= 2) primes.push_back(2);
+
+//     // * Mark all even numbers > 2 as non-prime
+//     for (long long i = 4; i <= n; i += 2)
+//         is_prime[i] = false;
+
+//     for (long long i = 3; i <= n; i += 2) {
+//         if (is_prime[i]) {
+//             primes.push_back(i);
+            
+//             if (i * i <= n) {
+//                 for (long long j = i * i; j <= n; j += 2 * i)
+//                     is_prime[j] = false;
+//             }
+//         }
+//     }
+
+//     return primes;
+// }
+
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
