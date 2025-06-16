1641b863b4c6297badfbaf0e8b8042ef1d686f74
CryptedNun <hasanmahmudbd2005@gmail.com>
2025-06-16 14:18:36 +0600

    Add algorithm for primes in range

--- CP-Algorithms/PrimeAlgorithms/SievePrimesInRange.cpp
+++ CP-Algorithms/PrimeAlgorithms/SievePrimesInRange.cpp
@@ -1,16 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<long long> sieveRange(long long l, long long h) {
-
-}
-
-// Classic sieve up to sqrt(n)
+//! Classic sieve up to floor-sqrt(n)
 vector<long long> simpleSieve(long long n) {
     vector<bool> is_prime(n + 1, true);
-    vector<long long> primes;
     is_prime[0] = is_prime[1] = false;
 
+    vector<long long> primes;
     // Handle 2 separately
     if (n >= 2) primes.push_back(2);
 
@@ -19,18 +15,36 @@ vector<long long> simpleSieve(long long n) {
         is_prime[i] = false;
 
     // Process only odd numbers
-    for (long long i = 3; i <= n; i += 2) {
+    for (long long i = 3; i*i <= n; i += 2) {
         if (is_prime[i]) {
             primes.push_back(i);
+
+            for (long long j = i * i; j <= n; j += 2 * i)
+                is_prime[j] = false;
             
-            // Mark odd multiples of i as non-prime
-            if (i * i <= n) {
-                for (long long j = i * i; j <= n; j += 2 * i)
-                    is_prime[j] = false;
-            }
         }
     }
     return primes;
+}
+
+//! Returns the primes within l--h range
+vector<long long> sieveRange(long long l, long long h) {
+    vector<long long> primes_h = simpleSieve(h);
+    vector<bool> range(h-l+1, true);
+
+    for (long long p : primes_h) {
+        long long start = max(p * p, ((l + p - 1) / p) * p);
+        for (long long j = start; j <= h; j += p) {
+            range[j - l] = false;
+        }
+    }
+
+    vector<long long> result;
+    for (long long i = 0; i <= h - l; ++i) {
+        if (range[i] && (l + i) > 1)
+            result.push_back(l + i);
+    }
+    return result;
 }
 
 int main() {
@@ -38,7 +52,6 @@ int main() {
     cin.tie(nullptr);
 
     long long l, h; cin>>l>>h;
+    vector<long long> primes = sieveRange(l, h);
+    for(long long& num: primes) cout<<num<<" ";
 }
-
-// Get primes from 2 to floorroot(h)
-// Cross multiples of those primes in the l -- h range (inclusive)
